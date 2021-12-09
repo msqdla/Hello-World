5474d3d9d4f67d6102622a61acd17b316d4ae029
LaoDa <62365942+msqdla@users.noreply.github.com>
2021-12-09 14:52:27 +0800

    Update Text Outlines.py

--- Text Outlines.py
+++ Text Outlines.py
@@ -7,8 +7,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patheffects import Stroke, Normal
 
-fig = plt.figure(figsize=(8, 3))
+fig = plt.figure(figsize=(8, 3)) #figsize 是一个属性，因此不用去管他，只需要知道它能起到什么作用就好，它就是用来定义你的图像的大小。这句话的意思就是作为plt的一个方法的figure。
 ax = fig.add_axes([0, 0, 1, 1], frameon=False)
+
+gs = fig.add_gridspec(2, 2)
+ax1 = fig.add_subplot(gs[0, 0])
+ax2 = fig.add_subplot(gs[1, 0])
+# spans two rows:
+ax3 = fig.add_subplot(gs[:, 1])
+
 family = "Pacifico"
 size = 100
 cmap = plt.cm.magma
